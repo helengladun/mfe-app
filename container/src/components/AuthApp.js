9fbf9c5a1369cdff5cb0439751b882f7a030721c
helengladun <helen.gladun@gmail.com>
2021-01-17 21:03:40 +0200

    add auth app

--- container/src/components/AuthApp.js
+++ container/src/components/AuthApp.js
@@ -1,8 +1,9 @@
 import React, {useRef, useEffect} from "react";
-import {mount} from 'marketing/MarketingApp';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-export default () => {
+import {mount} from 'auth/AuthApp';
+
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const {push, location, listen} = useHistory();
 
@@ -15,7 +16,8 @@ export default () => {
         if (pathname !== nextPathname) {
           push(nextPathname);
         }
-      }
+      },
+      onSignIn
     });
 
     listen(onParentNavigate);
